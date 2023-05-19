d70a13e1dc5fa2afc0cea162bce5492b302734f9
Ítalo Marcelo <youngitne@gmail.com>
2023-05-19 16:57:51 -0300

    primeira versão do site com html, css e js

---
+++ script.js
@@ -0,0 +1,7 @@
+function overZap(){
+    document.getElementById('zappopup').src = 'projeto pix/icons8-whatsapp-48-2.png'
+}
+
+function outZap(){
+    document.getElementById('zappopup').src = 'projeto pix/icons8-whatsapp-48.png'
+}
